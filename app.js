014b4eab1a2fe4aa9cf39a864043c25be9d0973d
Brack Carmony <brackcarmony@gmail.com>
2016-06-05 11:00:42 -0600

    updating moble support

--- app.js
+++ app.js
@@ -1,109 +1,140 @@
 var messageArray  = [
-  {file:"console",message:"This is a test of the emergency website broadcast system"},
-  {file:"style",message:"* {transition:.11s}"},
-  {file:"style",message:"body, html{padding:0;margin:0}"},
-  {file:"style",message:"#console {background-color:rgba(25,25,25,.6);"},
-  {file:"style",message:"color:white;"},
-  {file:"style",message:"font-size:8px;"},
-  {file:"style",message:"font-family:Monaco, monospaced;"},
-  {file:"style",message:"position:absolute;"},
-  {file:"style",message:"top:0;"},
-  {file:"style",message:"right:0;"},
-  {file:"style",message:"overflow:scroll;"},
-  {file:"style",message:"height:95%;"},
-  {file:"style",message:"width:50%;"},
-  {file:"style",message:"perspective:500px;"},
-  {file:"style",message:"transform:rotate3d(0, 38, 0, 30deg)}"},
-  {file:"console",message:"There, Now we have some room to work."},
+  {display:"", file:"console", message:"This is a test of the emergency website broadcast system"},
+  {display:"", file:"console", message:"Seems like there`s not much here."},
+  {display:"", file:"console", message:"Well let`s change that."},
+  {display:"", file:"style", message:"* {transition:1s}"},
+  {display:"", file:"style", message:"body, html{padding:0;margin:0}"},
+  {display:"", file:"style", message:"#console {background-color:Ωrgba(25,25,25,.6)Ω;"},
+  {display:"", file:"style", message:"color:white;"},
+  {display:"", file:"style", message:"font-size:.75em;"},
+  {display:"", file:"style", message:"font-family:ΩMonaco, monospacedΩ;"},
+  {display:"", file:"style", message:"position:absolute;"},
+  {display:"wide", file:"style", message:"top:0;"},
+  {display:"tall", file:"style", message:"bottom:0;"},
+  {display:"wide", file:"style", message:"right:0;"},
+  {display:"tall", file:"style", message:"left:0;"},
+  {display:"", file:"style", message:"overflow:scroll;"},
+  {display:"wide", file:"style", message:"height:95%;"},
+  {display:"tall", file:"style", message:"height:30%;"},
+  {display:"wide", file:"style", message:"width:50%;"},
+  {display:"tall", file:"style", message:"width:100%;"},
+  {display:"", file:"style", message:"perspective:Ω500px;Ω"},
+  {display:"", file:"style", message:"transform:rotate3dΩ(0, 38, 0, 30deg)Ω}"},
+  {display:"", file:"console", message:"There, Now we have some room to work."},
 
-  {file:"index",message:"<h1>Brack Carmony</h1>"},
-  {file:"index",message:"<h2>Web Developer</h2>"},
-  {file:"index",message:"<p>I'm a math nerd turned programmer.  I'm really not all that fond of talking about myself.  But I saw <a href='http://strml.net/'>this webpage</a> and thought it looked pretty fun to try and emulate.  Sure I probably shouldn't link to a site that does this same sort of thing better than me...  There's a reason I don't work in marketing. </p>"},
-  {file:"style",message:"*{box-sizing:borderbox}"},
-  {file:"style",message:"p{padding-left:30px;"},
-  {file:"style",message:" font-size:10px;"},
-  {file:"style",message:" text-align:justify-content;"},
-  {file:"style",message:" text-indent:15px;"},
-  {file:"style",message:" width:45%;}"},
+  {display:"", file:"index", message:"<h1>Brack Carmony</h1>"},
+  {display:"", file:"index", message:"<h2>Web Developer</h2>"},
+  {display:"", file:"index", message:"<p>I`m a math nerd turned programmer.  I`m really not all that fond of talking about myself.  But I saw <a href='http://strml.net/'>this webpage</a> and thought it looked pretty fun to try and emulate.  Sure I probably shouldn`t link to a site that does this same sort of thing better than me...  There`s a reason I don`t work in marketing. </p>"},
+  {display:"", file:"style", message:"*{box-sizing:borderbox}"},
+  {display:"wide", file:"style", message:"p{padding-left:30px;"},
+  {display:"tall", file:"style", message:"p{"},
+  {display:"", file:"style", message:" font-size:10px;"},
+  {display:"", file:"style", message:" text-align:Ωjustify;"},
+  {display:"", file:"style", message:" text-indent:Ω15pxΩ;"},
+  {display:"", file:"style", message:" text-indent:Ω15pxΩ;"},
 
-  {file:"style",message:"#index{overflow:auto;"},
-  // {file:"style",message:"background-color:#A9FFD4;"},
-  {file:"style",message:"color:#101915;"},
-  {file:"style",message:'font-family: "Palatino", Times, serif;}'},
+  {display:"tall", file:"style", message:" width:95%;"},
+  {display:"tall", file:"style", message:" margin:Ω0 auto 10px autoΩ;}"},
+  {display:"wide", file:"style", message:" width:Ω45%Ω;}"},
 
-  {file:"style",message:'h1{margin:0px;'},
-  {file:"style",message:'padding:10px;'},
-  {file:"style",message:'background-color:darkslategrey;'},
-  {file:"style",message:'color:mintcream}'},
-  {file:"style",message:'h2{padding:5px;'},
-  {file:"style",message:'margin:0px;'},
-  {file:"style",message:'padding-left:30px;}'},
-  {file:"style",message:'h1,h2{font-family:"Impact","Arial Black",sans-serif;}'},
-  {file:"console",message:"Okay, so maybe colors aren't my strong suit..."},
-  {file:"console",message:"I know, let's go with some high rez images..."},
-  {file:"console",message:"Those are in right?"},
+  {display:"", file:"style", message:"#index{overflow:auto;"},
+  // {display:"", file:"style", message:"background-color:#A9FFD4;"},
+  {display:"", file:"style", message:"color:#101915;"},
+  {display:"", file:"style", message:'font-family: Ω"Palatino", Times, serifΩ;}'},
+  {display:"", file:"style", message:'h1,h2{display:"", font-family:Ω"Impact","Arial Black",sans-serifΩ;}'},
+  {display:"", file:"style", message:'h1{margin:0px;'},
+  {display:"", file:"style", message:'padding:10px;'},
+  {display:"", file:"style", message:'background-color:darkslategrey;'},
+  {display:"", file:"style", message:'color:mintcream}'},
+  {display:"", file:"style", message:'h2{padding:Ω5px 5px 5px 30pxΩ;'},
+  {display:"", file:"style", message:'margin:0px;}'},
 
-  {file:"index",message:"<div click='globalJavaFunction' class='background-image leaf'><div class = 'picture-button'>Leafy Green</div></div>"},
-  {file:"index",message:"<div class='background-image marble'><div class = 'picture-button'>Marble Grey</div></div>"},
-  {file:"index",message:"<div class='background-image twilight'><div class = 'picture-button'>Twilight Ghost</div></div>"},
-  {file:"style",message:'.leaf{background-image:url(leaf.jpg);}'},
-  {file:"style",message:'.marble{background-image:url(marble.jpg);}'},
-  {file:"style",message:'.twilight{background-image:url(twilight.jpg);}'},
-  {file:"style",message:'#index{height:100vh;'},
-  {file:"style",message:'overflow:scroll;}'},
-  {file:"style",message:'.background-image{'},
-  {file:"style",message:'background-size:cover;'},
-  {file:"style",message:'background-position:center;'},
-  {file:"style",message:'position:relative;'},
-  {file:"style",message:'height:45%;}'},
+  {display:"", file:"console", message:"Okay, so maybe colors aren't my strong suit..."},
+  {display:"", file:"console", message:"I know, let's go with some high rez images..."},
+  {display:"", file:"console", message:"Those are in right?"},
 
-  {file:"style",message:'.picture-button{color:white;'},
-  {file:"style",message:'display:inline-block;'},
-  {file:"style",message:'position:absolute;'},
-  {file:"style",message:'border:1px solid white;'},
-  {file:"style",message:'border-radius:4px;'},
-  {file:"style",message:'padding:15px;'},
+  {display:"wide", file:"index", message:"<div click='globalJavaFunction' class='background-image leaf'><div class = 'picture-button'>Leafy Green</div></div>"},
+  {display:"wide", file:"index", message:"<div class='background-image marble'><div class = 'picture-button'>Marble Grey</div></div>"},
+  {display:"wide", file:"index", message:"<div class='background-image twilight'><div class = 'picture-button'>Twilight Ghost</div></div>"},
+  {display:"tall", file:"index", message:"<div click='globalJavaFunction' class='background-image leaf'><div class = 'picture-button'>Leaf</div></div>"},
+  {display:"tall", file:"index", message:"<div class='background-image marble'><div class = 'picture-button'>Marble</div></div>"},
+  {display:"tall", file:"index", message:"<div class='background-image twilight'><div class = 'picture-button'>Twilight</div></div>"},
+  {display:"", file:"style", message:'.leaf{background-image:url("leaf.jpg");}'},
+  {display:"", file:"style", message:'.marble{background-image:url("marble.jpg");}'},
+  {display:"", file:"style", message:'.twilight{background-image:url("twilight.jpg");}'},
+  {display:"", file:"style", message:'#index{height:100vh;'},
+  {display:"", file:"style", message:'overflow:scroll;}'},
+  {display:"", file:"style", message:'.background-image{'},
+  {display:"", file:"style", message:'background-size:cover;'},
+  {display:"", file:"style", message:'background-position:center;'},
 
-  {file:"style",message:'margin:0;'},
-  {file:"style",message:'top:50%;'},
-  {file:"style",message:'left:50%;'},
-  {file:"style",message:'margin-right:-50%;'},
-  {file:"style",message:'transform:translate(-50%, -50%);}'},
+  {display:"", file:"style", message:'position:relative;'},
+  {display:"", file:"style", message:'height:1em;'},
+  {display:"wide", file:"style", message:'height:45%;}'},
+  {display:"tall", file:"style", message:'height:Ω33.33333vwΩ;}'},
 
-  {file:"style",message:'.background-image{'},
-  {file:"style",message:'display:inline-block;'},
-  {file:"style",message:'width:calc(100% / 3)}'},
-
-  {file:"console",message:"I suppose I should include those social links..."},
-
-  {file:"index",message:"<div class = 'icons'> "},
-  {file:"index",message:'<a href = "https://github.com/kedirech/"><i class="fa fa-github-square" aria-hidden="true"></i></a>'},
-  {file:"index",message:'<a href = "https://www.facebook.com/brackcarmony"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>'},
-  {file:"index",message:'<a href = "https://twitter.com/BrackCarmony"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>'},
+  {display:"", file:"style", message:'.picture-button{color:white;'},
+  {display:"", file:"style", message:'display:Ωinline-blockΩ;'},
+  {display:"", file:"style", message:'position:absolute;'},
+  {display:"", file:"style", message:'border:Ω1px solid whiteΩ;'},
+  {display:"", file:"style", message:'padding:15px;'},
 
 
-  {file:"style",message:'.icons{'},
-  {file:"style",message:'font-size:10vw;'},
-  {file:"style",message:'display:flex;'},
-  {file:"style",message:'width:50%;'},
-  {file:"style",message:'justify-content:space-around;'},
-  {file:"style",message:'}'},
-  {file:"style",message:'.icons a{color:black}'},
+  {display:"", file:"style", message:'margin:0;'},
+  {display:"", file:"style", message:'top:0;'},
+  {display:"", file:"style", message:'top:Ω50%Ω;'},
+  {display:"", file:"style", message:'left:0;'},
+  {display:"", file:"style", message:'left:Ω50%Ω;'},
+  {display:"", file:"style", message:'margin-right:Ω-50%Ω;'},
+  {display:"", file:"style", message:'transform:Ωtranslate(-50%, -50%)Ω;'},
+  {display:"", file:"style", message:'border-radius:6px;}'},
 
-  {file:"console",message:"Feel free to poke around the source code..."},
-  {file:"index",message:'<a href = "https://github.com/Kedirech/self-build"><i class="fa fa-code" aria-hidden="true"></i></a>'},
-  {file:"console",message:"or shoot me an email."},
-  {file:"index",message:'<a href = "mailto:brackcarmony@gmail.com"><i class="fa fa-envelope" aria-hidden="true"></i></a>'},
-  {file:"console",message:"comments are always appreciated."},
-  {file:"index",message:"</div> "},
+  {display:"", file:"style", message:'.background-image{'},
+
+  {display:"", file:"style", message:'width:Ω100%Ω;'},
+  {display:"", file:"style", message:'width:Ωcalc(100% / 3)Ω;'},
+  {display:"", file:"style", message:'display:Ωinline-blockΩ;}'},
+
+  {display:"", file:"console", message:"I suppose I should include those social links..."},
+
+  {display:"", file:"index", message:"<div class = 'icons'>"},
+  {display:"", file:"index", message:'<a href = "https://github.com/kedirech/"><i class="fa fa-github-square" aria-hidden="true"></i></a>'},
+  {display:"", file:"index", message:'<a href = "https://www.facebook.com/brackcarmony"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>'},
+  {display:"", file:"index", message:'<a href = "https://twitter.com/BrackCarmony"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>'},
+  {display:"", file:"index", message:'<a href = "https://github.com/Kedirech/self-build"><i class="fa fa-code" aria-hidden="true"></i></a>'},
+  {display:"", file:"index", message:'<a href = "mailto:brackcarmony@gmail.com"><i class="fa fa-envelope" aria-hidden="true"></i></a>'},
+  {display:"", file:"index", message:"</div> "},
+  {display:"", file:"style", message:'.icons a{color:ΩblackΩ}'},
+  {display:"", file:"style", message:'.icons a:hover{color:ΩblueΩ;'},
+  {display:"", file:"style", message:'cursor:ΩpointerΩ;}'},
+
+  {display:"", file:"style", message:'.icons{'},
+  {display:"", file:"style", message:'display:Ωflex;Ω'},
+  {display:"", file:"style", message:'justify-content:Ωspace-around;Ω'},
+  {display:"", file:"style", message:'font-size:Ω10vw;Ω'},
+  {display:"wide", file:"style", message:'width:Ω50%;Ω'},
+  {display:"tall", file:"style", message:'width:Ω100%;Ω'},
+  {display:"", file:"style", message:'}'},
+
+
+
+
+  {display:"", file:"console", message:"Feel free to poke around the source code..."},
+
+  {display:"", file:"console", message:"or shoot me an email."},
+
+  {display:"", file:"console", message:"comments are always appreciated."},
+
 
 ]
 
-var spd = .00010;
+var spd = 10;
 var lineIndex=0;
 var letterIndex=0;
 var target;
+
 var cons = document.getElementById("console");
+var display = window.innerHeight>window.innerWidth?"tall":"wide";
 var fileType = messageArray[0].file;
 var files = {
   index:"",
@@ -113,23 +144,30 @@ var files = {
 }
 
 function nextLetter() {
+  var letters = getLetter().replace(/`/g,"'").replace(/Ω/g,"");
 
-  files.console += messageArray[lineIndex].message[letterIndex]=="<"?"&lt;":messageArray[lineIndex].message[letterIndex];
+  files.console += letters.replace("<","&lt")//messageArray[lineIndex].message[letterIndex]=="<"?"&lt;":messageArray[lineIndex].message[letterIndex];
   cons.innerHTML = files.console;
-  console.log(cons)
+  // console.log(cons)
   cons.scrollTop = 100000;
   if (target){
-    console.log(fileType);
-    console.log(target);
-    files[fileType] += messageArray[lineIndex].message[letterIndex];
-    console.log(files);
+    // console.log(fileType);
+    // console.log(target);
+    files[fileType] += letters;
+    // console.log(files);
     target.innerHTML = files[fileType];
   }
   letterIndex++;
 
-  if (letterIndex == messageArray[lineIndex].message.length){
+  if (letterIndex >= messageArray[lineIndex].message.length){
     letterIndex = 0;
     lineIndex++;
+    console.log(messageArray[lineIndex].display);
+    while (messageArray[lineIndex].display && messageArray[lineIndex].display !=display){
+      console.log(display,messageArray[lineIndex].display);
+      console.log("skipping", messageArray[lineIndex].message);
+      lineIndex++;
+    }
     if (lineIndex == messageArray.length) return;
 
     fileType = messageArray[lineIndex].file;
@@ -142,3 +180,36 @@ function nextLetter() {
 }
 
 nextLetter();
+
+function getLetter(){
+
+  var match ="";
+  switch(messageArray[lineIndex].message[letterIndex]){
+    case "Ω":
+
+    match = "Ω";
+    break;
+    case "'":
+
+    match = "'";
+    break
+    case "<":
+
+    match = ">";
+    break
+    case '"':
+
+    match = '"';
+    break
+
+    default:
+    return messageArray[lineIndex].message[letterIndex];
+  }
+  var retStr = messageArray[lineIndex].message[letterIndex++];
+   while (messageArray[lineIndex].message[letterIndex] != match && messageArray[lineIndex].message[letterIndex]){
+     retStr += messageArray[lineIndex].message[letterIndex++]
+   }
+  //  letterIndex++;
+
+   return retStr + match;
+}
